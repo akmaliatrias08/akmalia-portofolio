876ac3b474861e79c7be51846899194e729021e1
akmaliatrias08 <akmaliatrias08@gmail.com>
2025-03-02 16:43:27 +0700

    first commit

---
+++ assets/js/biodata.js
@@ -0,0 +1,31 @@
+const biodata = {
+    avatar: "./assets/images/avatar-2.png",
+    name: "Akmalia Trias Agustin", 
+    job: "Student", 
+    contactList: [
+        {
+            icon: `<ion-icon name="mail-outline"></ion-icon>`,
+            title: "Email", 
+            info: "akmaliatrias08@gmail.com", 
+            link: "mailto:akmaliatrias08@gmail.com"
+        }, 
+        {
+            icon: `<ion-icon name="call-outline"></ion-icon>`,
+            title: "Phone", 
+            info: "(+62) 896-3765-0752", 
+            link: "https://wa.me/+6289647650752"
+        }, 
+        {
+            icon: `<ion-icon name="calendar-outline"></ion-icon>`,
+            title: "Birthday", 
+            info: "August 13, 2004", 
+            link: "#"
+        }, 
+        {
+            icon: `<ion-icon name="location-outline"></ion-icon>`,
+            title: "Location", 
+            info: "Kota Bekasi", 
+            link: "#"
+        }
+    ]
+}
